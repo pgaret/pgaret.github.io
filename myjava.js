9d48c8767c9a9146868c219cb50826660b493f8f
pgaret <peregringaret@gmail.com>
2016-06-21 19:01:25 -0400

    Quick Changes
    Updated some minor things

--- myjava.js
+++ myjava.js
@@ -3,9 +3,8 @@ var old = null;
 function details(param){
 	if (old != null){
 		document.getElementById(old).style.visibility = "hidden";
-		if (old == param) return;
 	}
-	old = param;
+
 	document.getElementById(param).style.visibility = "visible";
 	if (param == "MM"){
 		document.getElementById("MM").innerHTML = "Myths and Magic: <br> <ul><li>Fantasy RPG</li><li>3 Month Development Period</li><li>Roles: Lead, Designer, Writer</li>";		
@@ -16,4 +15,6 @@ function details(param){
 	if (param == "Asylum"){
 		document.getElementById("Asylum").innerHTML = "Asylum: <br> <ul><li>Psychological Thriller</li><li>Used Oculus Rift VR<li>3 Month Development Period</li><li>Roles: Lead, Designer</li>";		
 	}
+	
+	old = param;
 }
