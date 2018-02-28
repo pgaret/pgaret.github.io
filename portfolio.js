6e547987531725a408d5e2c9126e7f713bef0672
Peregrin Garet <pgaret@Peregrins-MacBook-Air.local>
2018-02-28 15:45:00 -0500

    Removing unnecessary flair elements

--- portfolio.js
+++ portfolio.js
@@ -2,42 +2,6 @@ var name = "PEREGRIN GARET";
 var currentPopup = "";
 var currentSlide = 0;
 
-$(document).ready(function(){
-	setTimeout(function fillName(){
-		let index = $("#title").text().length; let str = ""
-		if (name[index] === " " && index < name.length) { str = name[index]+name[index+1] }
-		else { str = name[index] }
-		$("#title").append(str);
-		if ($("#title").text().length < name.length){
-			setTimeout(fillName, 100);
-		}
-		else {
-			fadeInChildren("skills", spawnIcons)
-		}
-	}, 100)
-})
-
-function fadeInChildren(str, callback){
-	let i = 0;
-	setTimeout(function show(){
-		if (i !== 0) { $($("#"+str).children()[i]).css("display", "inline-block") }
-		$($("#"+str).children()[i]).animate({
-			opacity: 1
-		}, 500, function(){
-					i++
-					if (i < $("#"+str).children().length){
-						setTimeout(show, 100);
-					}
-					else {
-						if (callback) {callback()}
-					}
-				})
-	}, 100)
-}
-
-function spawnIcons(){
-	fadeInChildren("icons");
-}
 
 function openPopup(id){
 	$(".popup").css("display", "block");
